77b18186a8196405f80ad891e0fa06d9dd29cb19
Mez <mez@jetcode.io>
2016-06-08 13:44:27 -0400

    try to fix spacing issues.

--- app/pages/github/githubrepo.ts
+++ app/pages/github/githubrepo.ts
@@ -10,35 +10,34 @@ import {Repo} from "./repo";
       <div text-center>{{repo.name}} </div>
     </ion-card-header>
 
-			<ion-item>
-				<ion-row>
-		    	<ion-col width-25>
-				    <button dark clear item-left padding-left>
-				      <ion-icon style="font-size:1.8em;" name="star"></ion-icon>
-				      <div>{{repo.stargazers_count}}</div>
-				    </button>
-					</ion-col>
-					<ion-col width-25>
-				    <button dark clear item-left>
-				      <ion-icon style="font-size:1.8em;" name="eye"></ion-icon>
-				      <div>{{repo.watchers}}</div>
-				    </button>
-				  </ion-col>
-				  <ion-col width-25>
-				    <button dark clear item-left>
-				      <ion-icon style="font-size:1.8em;" name="information-circle-outline"></ion-icon>
-				      <div>{{repo.open_issues}}</div>
-				    </button>
-				  </ion-col>
-				  <ion-col width-25>
-				    <button dark clear item-left>
-				      <ion-icon style="font-size:1.8em;" name="git-network"></ion-icon>
-				      <div>{{repo.forks}}</div>
-				    </button>
-				  </ion-col>
-				</ion-row>
-			</ion-item>
-				
+		<ion-item>
+			<ion-row>
+	    	<ion-col width-25>
+			    <button dark clear item-left padding-left>
+			      <ion-icon style="font-size:1.8em;" name="star"></ion-icon>
+			      <div>{{repo.stargazers_count}}</div>
+			    </button>
+				</ion-col>
+				<ion-col width-25>
+			    <button dark clear item-left>
+			      <ion-icon style="font-size:1.8em;" name="eye"></ion-icon>
+			      <div>{{repo.watchers}}</div>
+			    </button>
+			  </ion-col>
+			  <ion-col width-25>
+			    <button dark clear item-left>
+			      <ion-icon style="font-size:1.8em;" name="information-circle-outline"></ion-icon>
+			      <div>{{repo.open_issues}}</div>
+			    </button>
+			  </ion-col>
+			  <ion-col width-25>
+			    <button dark clear item-left>
+			      <ion-icon style="font-size:1.8em;" name="git-network"></ion-icon>
+			      <div>{{repo.forks}}</div>
+			    </button>
+			  </ion-col>
+			</ion-row>
+		</ion-item>			
   </ion-card>
 	`
 })
